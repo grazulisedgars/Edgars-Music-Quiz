72fd899f9f184ce57226cab330e997f75e66971a
grazulisedgars <grazulisedgars@gmail.com>
2023-11-04 13:21:05 +0200

    Adds timer and setTime function

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,12 +1,31 @@
-// This JS file should have files about games functionality
-
-
-//The first view of the application displays a button to start the quiz (I guess this is already done by starter code?)
-
+var buttonEl = document.querySelector("#start");
+var questionsEl = document.querySelector("#questions");
+var choicesEl = document.querySelector("#choices");
+var timeEl = document.querySelector(".timer");
 
 // Clicking the “Start the Quiz” button displays a series of question 
 
 // Once the quiz begins, a countdown timer starts 
+
+var secondsLeft = 60;
+
+function setTime() {
+    // Sets interval in variable
+    var timerInterval = setInterval(function () {
+        secondsLeft--;
+        timeEl.textContent = secondsLeft;
+
+        if (secondsLeft === 0) {
+            // Stops execution of action at set interval
+            clearInterval(timerInterval);
+            timeEl.textContent = " ";
+            // Calls function to create and append image
+            sendMessage();
+        }
+
+    }, 1000);
+}
+// setTime();
 
 // If a question is answered incorrectly, additional time is subtracted from the timer 
 
@@ -26,3 +45,12 @@
 // THEN the game is over
 // WHEN the game is over
 // THEN I can save my initials and score
+
+//iterate over the list of questions 
+//if a user clicked on an answer 
+//compare the value to the correct answer 
+// if the thing user answered is correct 
+// display correct
+//else
+//display incorrect 
+//subtract time 
